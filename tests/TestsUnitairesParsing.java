c8e732692488de0b04b3bdbef7910bb4ebdf119c
vivingo <ngo.virginie1@yahoo.fr>
2016-01-25 01:35:27 +0100

    Update files

--- tests/TestsUnitairesParsing.java
+++ tests/TestsUnitairesParsing.java
@@ -16,16 +16,40 @@ abstract class B extends C {
 	    int c = 18;
 	    { ; }
 	    new A();
+	    A.m();
+	    a[2].m();
+	    a[2][6].m();
+	    B.A.m();
+	    this.m();
+	    super();
+	    this();
+	    super.m();
+	    m();
 	    for (int c; c > 6 ; c++) { }
 	}
-	//	tutu.tyty.a = 1;
+	tutu.tyty.a = 1;
 	final tutu.tyty.a op;
 	a(B.this);
 	tutu.a op;
 	a = b = 1;
 	return new A();
+	try {
+	} catch (E e) {
+	    toto;
+	} finally {
+	    tutu;
+	}
+	try {
+	} finally {
+	    tutu;
+	}
+	try {
+	} catch (E e) {
+	    toto;
+	}
     }
 
     Int matt4() { this.ab; }
 }
 static private class C extends A {}
+
