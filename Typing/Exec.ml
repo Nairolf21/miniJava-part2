f3d4584b46eb758f3c2cb11f35e30a75f01a763e
yannlefoll125 <yann.lefoll@telecom-bretagne.eu>
2016-02-12 19:40:15 +0100

    Exec.class compilation: fixed class compilation

--- Typing/Exec.ml
+++ Typing/Exec.ml
@@ -27,26 +27,19 @@ type memory =
  * create_class_desc_list. *)
 (* astclass -> string -> class_desc *)
 let create_class_desc astclass id = 
-    print_endline "create_class_desc";
     let namelist = List.map (fun attr -> attr.aname) astclass.cattributes in
     let methodlist= List.map (fun meth -> meth.mname) astclass.cmethods in
     { name = id; attributes = namelist; method_names = methodlist }
 
 (* AST.astclass -> string -> compiled_method list *)
-let rec add_methods astclass id_class meth_table =
-    print_endline "add_methods";
-    match astclass.cmethods with 
+let rec add_methods class_meth_list id_class meth_table =
+    match class_meth_list with 
     | [] -> meth_table
-    | h :: t -> add_methods astclass id_class (meth_table @ [create_comp_method id_class h])
+    | h :: t -> add_methods t id_class (meth_table @ [create_comp_method id_class h])
 
-and add_methods
+(*and add_methods*)
 
 and create_comp_method id_class astmethod =
-    print_endline "create_comp_method";
-    print_string "id_class: ";
-    print_endline id_class;
-    print_string "meth_name:";
-    print_endline astmethod.mname;
     {
         comp_name = id_class^"_"^astmethod.mname;
         meth = astmethod
@@ -62,7 +55,7 @@ let rec add_classes_to_memory type_list mem =
 (* AST.t -> string -> memory *)
 and add_class_to_memory astclass id_class mem =
     {
-        meth_table =  add_methods astclass id_class mem.meth_table;
+        meth_table =  add_methods astclass.cmethods id_class mem.meth_table;
         class_desc_list = mem.class_desc_list @ [create_class_desc astclass id_class]
     }
 
@@ -80,7 +73,8 @@ let print_class_desc cd =
     List.iter (fun el -> print_endline el) cd.method_names
 
 let print_class_desc_list cdl =
-    print_endline "Class descriptions";
+    print_endline "";
+    print_endline "Class descriptors";
     print_endline "";
     List.iter (function el -> print_class_desc el) cdl;
     print_endline ""
@@ -88,7 +82,8 @@ let print_class_desc_list cdl =
 let print_method_table_entry entry =
     print_string entry.comp_name;
     print_string ": ";
-    print_string entry.meth.mname
+    print_string entry.meth.mname;
+    print_endline ""
 
 let print_method_table meth_table =
     print_endline "Method table (comp_name: ast_name)";
@@ -97,6 +92,12 @@ let print_method_table meth_table =
     print_endline ""
 
 let print_memory mem = 
+    print_endline "";
+    print_endline "Printing memory representation";
+    print_endline "";
     print_method_table mem.meth_table;
-    print_class_desc_list mem.class_desc_list
+    print_class_desc_list mem.class_desc_list;
+    print_endline "";
+    print_endline "End of memory representation";
+    print_endline ""
 
