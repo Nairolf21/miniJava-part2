7d184cd3f1aee489b6ff0d2f94a084ec126cf42a
Florian Coste <fcoste21@gmail.com>
2016-02-18 00:23:33 +0100

    TypeAST: Catch packages and type_list types

--- Typing/TypeAST.ml
+++ Typing/TypeAST.ml
@@ -1,5 +1,21 @@
 open AST
 open Type
 
+(* packages seem not be stored by the parser
+ * Maybe it's not necessary to verify packages *)
+let check_package package = 
+    match package with
+    | Some p -> print_endline "package found"
+    | None -> print_endline "no package found"
+    | _ -> print_endline "default result"
+
+let check_type_list type_list = 
+    match type_list with 
+    | [] -> print_endline "no type_list"
+    | h::t -> print_endline "list found"
+    | _ -> print_endline "default result"
+
 let typing ast = 
-    print_endline "type-checking in progress"
+    print_endline "type-checking in progress";
+    check_type_list ast.type_list;
+    check_package ast.package
