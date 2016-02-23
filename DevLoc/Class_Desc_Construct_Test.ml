a939da8e031a101fbf78f298133ddc8d175f8392
yannlefoll125 <yann.lefoll@telecom-bretagne.eu>
2016-02-23 22:39:21 +0100

    Class_Dec_Construct_Test: succesfully constructed the method name map of a child class desc from parent class desc and child method name list. Now need to add corresponding new method table entries, and integrate to the application"

--- DevLoc/Class_Desc_Construct_Test.ml
+++ DevLoc/Class_Desc_Construct_Test.ml
@@ -20,24 +20,20 @@ let construct_meth_table_key method_name parent_mmap child_id child_mlist =
     else StringMap.find method_name parent_mmap;;
 
 
-
-let add_parent_methods parent_mmap child_mlist parent_id child_id =
-    let rec add_parent_methods_rec parent_mlist child_mlist child_mmap =
-        print_endline "parent_mlist";
-        List.iter (fun el -> print_string (el^";")) parent_mlist;
-        print_endline "";
-        print_endline "child_mlist";
-        List.iter (fun el -> print_string (el^";")) child_mlist;
-        print_endline "";
-        match parent_mlist with
+(*Construct method map of the child class descriptor based on the parent class descriptor and the child method name list*)
+let add_methods_to_child_desc parent_mmap child_mlist parent_id child_id =
+    let rec add_child_methods_rec child_mlist child_mmap =
+        match child_mlist with
         | [] -> child_mmap
+        | h :: t -> add_child_methods_rec t (StringMap.add h (full_mname h child_id) child_mmap)
+    in
+    let rec add_parent_methods_rec parent_mlist child_mlist child_mmap =
+        match parent_mlist with
+        | [] -> add_child_methods_rec child_mlist child_mmap
         | h :: t -> 
                 let new_child_list = ListII.remove h child_mlist in
-                print_endline "new_child_list: OK";
                 let method_table_key = construct_meth_table_key h parent_mmap child_id child_mlist in
-                print_endline "method_table_key: OK";
                 let updated_child_mmap = StringMap.add h method_table_key child_mmap in
-                print_endline "updated_child_mmap: OK";
                 add_parent_methods_rec t new_child_list updated_child_mmap 
     in
     add_parent_methods_rec (sm_list_keys parent_mmap) child_mlist StringMap.empty
@@ -51,13 +47,13 @@ let animal_mmap = StringMap.add "toString" "Object_toString" animal_mmap;;
 let animal_mmap = StringMap.add "walk" "Animal_walk" animal_mmap;;
 let animal_mmap = StringMap.add "run" "Animal_run" animal_mmap;;
 
-let cat_mlist = ["walk"; "run"; "meow"];;
+let cat_mlist = ["run"; "meow"];;
 
 print_endline "before";;
 print_endline "";;
 print_endline "animal_mmap";;
 sm_list_keys animal_mmap;;
 print_endline "";;
-let cat_mmap = add_parent_methods animal_mmap cat_mlist "Animal" "Cat";;
+let cat_mmap = add_methods_to_child_desc animal_mmap cat_mlist "Animal" "Cat";;
 print_endline "cat_mmap after adding parent methods";;
 StringMap.iter print_el cat_mmap;;
