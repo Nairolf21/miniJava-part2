9f28f0ef749e37e9ea3015e3aea39eb1e68dfc69
yannlefoll125 <yann.lefoll@telecom-bretagne.eu>
2016-02-24 05:23:09 +0100

    it compiles ! Lets test the behviour

--- Utils/ListII.mli
+++ Utils/ListII.mli
@@ -2,4 +2,3 @@ val concat_map : string -> ('a -> string) -> 'a list -> string
 val extract_last : 'a list -> 'a * 'a list
 val remove : 'a -> 'a list -> 'a list
 val is_in_list : 'a -> 'a list -> bool
-val dummy_list : string list
