bda50da987467f4918e597c40026ffa7ed3e3397
yannlefoll125 <yann.lefoll@telecom-bretagne.eu>
2016-02-24 03:06:02 +0100

    Updated ListII.mli

--- Utils/ListII.mli
+++ Utils/ListII.mli
@@ -1,6 +1,5 @@
-(* A call to concat_map sep f l produce the string concatenating the
-application of f to each element of l using sep to separate each
-element.  *)
-val concat_map: string -> ('a -> string) -> 'a list -> string
-							 
-val extract_last: 'a list -> 'a * 'a list
+val concat_map : string -> ('a -> string) -> 'a list -> string
+val extract_last : 'a list -> 'a * 'a list
+val remove : 'a -> 'a list -> 'a list
+val is_in_list : 'a -> 'a list -> bool
+val dummy_list : string list
