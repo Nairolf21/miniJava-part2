ce5e1518b1d2870b53eb8c18ebbee020c780c61d
vivingo <ngo.virginie1@yahoo.fr>
2016-02-25 22:26:10 +0100

    save changes before rebase with develop to integrate changes

--- Typing/TypeAST.ml
+++ Typing/TypeAST.ml
@@ -4,22 +4,39 @@ open Type
 
 
 (* Typing functions *)
+let add_class_list c env=
+	env @ c
 
-let typing exp =
+let type_type_info exp env = 
+	match exp.info with
+	| Class c -> add_class_list c env
+	| Inter -> ()
+
+let type_astype_info exp env =
+	match exp.info with
+	| Some info -> type_type_info exp env
+
+let rec type_program exp env=
+	match exp.type_list with
+	| None -> ()
+	| h::t -> type_asttype_info h env :: type_program t env
+
+let typing exp env =
 	match exp.package with
-	| None -> Null
-	| Some pack -> String(String.concat "." pack) 
+	| None -> type_program exp env
+	| Some pack -> type_program exp env
 
 
 (* Printing functions *)
-  
+(*   
 let rec print_tpackage p =
 	match p with 
 	| [] -> ()
-	| [h] -> print_endline ("package'"^h^"'")
-	| h::t -> print_string ("package'"^h^"'."); print_tpackage t
+	| [h] -> print_endline ("package"^h)
+	| h::t -> print_string ("package"^h^"."); print_tpackage t
 
 let print_typing exp =
   match exp.package with
   | None -> ()
   | Some pack -> print_tpackage pack ;
+ *)
