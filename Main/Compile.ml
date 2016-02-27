4e05fdce0178e0716a6214a9140e64266ab9ee69
vivingo <ngo.virginie1@yahoo.fr>
2016-02-27 01:05:35 +0100

    Merge Main/Compile file

--- Main/Compile.ml
+++ Main/Compile.ml
@@ -1,5 +1,6 @@
 open Parser
 open TypeAST
+open Compile_AST
 
 let execute lexbuf verbose = 
   try 
@@ -8,8 +9,9 @@ let execute lexbuf verbose =
     if verbose then AST.print_program ast ;
     let type_AST = TypeAST.typing ast [] in
     print_endline "successfull typing";
-
-
+    let memory = compile_ast ast in
+    print_endline "successful class compilation";
+    Memory_Model.print_memory memory;
 
   with 
     | Error ->
